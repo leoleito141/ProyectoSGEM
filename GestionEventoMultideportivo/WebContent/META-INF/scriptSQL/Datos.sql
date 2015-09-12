e31b940cd930d5ae443579c950df05d85f5a21cf
Leonardo <leonardo.cassanello@gmail.com>
2015-09-12 14:39:56 -0300

    Se agrega script para los datos iniciales, después vemos que colocar. Por ahora se inserta el admin.

---
+++ GestionEventoMultideportivo/WebContent/META-INF/scriptSQL/Datos.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM dbeventomultideportivo.usuario
+LIMIT 0, 1000
+
+-- Date: 2015-09-12 14:34
+*/
+INSERT INTO `usuario` (`DTYPE`,`id`,`Apellido`,`CanalYoutube`,`Cedula`,`Edad`,`Email`,`Facebook`,`Nombre`,`Password`,`Twitter`) VALUES ('Admin',1,'Admin','youtube',0,0,'admin@gmail.com','facebook','Admin','123','twitter');
