950d53c27ee76fa6f4a2c78ca1141456000fce7a
Juanma <Juanma@Juanma-PC>
2015-10-12 18:44:10 -0300

    Se da de alta los deportes o eventos deportivos le falta ponerle el tenantid y el id de evento multideportivo

--- GestionEventoMultideportivo/WebContent/resources/defecto/js/controllers/eventoDeportivo.js
+++ GestionEventoMultideportivo/WebContent/resources/defecto/js/controllers/eventoDeportivo.js
@@ -5,11 +5,34 @@ angular.module('pruebaAngularApp')
 	  $scope.eventoDeportivo={};
 	  console.log($scope.eventoDeportivo);
 	  
+	  
+	  $scope.openInicio = function($eventInicio) {
+		    $scope.statusInicio.opened = true;
+		  };
+
+	  $scope.openFin = function($eventFin) {
+		    $scope.statusFin.opened = true;
+		  };
+	   
+	  $scope.statusInicio = {
+			    opened: false
+			  };
+	  
+	  $scope.statusFin = {
+			    opened: false
+			  };
+	  
+	  $scope.dateOptions = {
+			    formatYear: 'yy',
+			    startingDay: 1
+			  };
+	  
+	  
 	  $scope.altaEventoDeportivo = function(){
 		  dataFactory.altaEventoDeportivo($scope.eventoDeportivo)
 	     	.then(function (data, status, headers, config) {
 	                $scope.status = data.status;
-	                console.log("Entre Alta Evento");
+	                console.log("Entre Alta Evento Deportivo");
 	                console.log(data.status);
 	                console.log(status);
 	                console.log(headers);
