7bd4546694e6a0a5b8dc7190d60bae1091dadc51
BeFx <BeFx@BeFx-PC>
2015-10-17 03:03:35 -0200

    Carga de noticias pasada a script, falta generacion automatica de columnas para diferenciar tamaños

--- GestionEventoMultideportivo/WebContent/resources/defecto/js/controllers/perfil.js
+++ GestionEventoMultideportivo/WebContent/resources/defecto/js/controllers/perfil.js
@@ -8,13 +8,53 @@ angular.module('pruebaAngularApp')
 
 	  $scope.confPerfil={};
 	  
+
+	  
+	  var Noticias = [
+	                   {"image":"http://1.bp.blogspot.com/-cejBwHSVGWU/UB8yOBAIGOI/AAAAAAAAAKQ/iFJ82hcjKBg/s1600/debora+rodriguez.jpg", "title":"Debora salio a correr","col":"8"},
+	                   {"image":"http://imagenes.montevideo.com.uy/imgnoticias/201507/_W620/513237.jpg", "title":"Plantel Titular de Uruguay","col":"4"},
+	                   {"image":"http://1.bp.blogspot.com/-cejBwHSVGWU/UB8yOBAIGOI/AAAAAAAAAKQ/iFJ82hcjKBg/s1600/debora+rodriguez.jpg", "title":"Debora Gano Medalla","col":"4"},
+	                   {"image":"http://imagenes.montevideo.com.uy/imgnoticias/201507/_W620/513237.jpg", "title":"Uruguay Campeon!!","col":"8"},
+	               ]; 
+	  function appendNews(col,image,titulo){
+		  
+		  var Html = "<div class='col-sm-5 col-md-"+col+"'> <div class='thumbnail'>"+"<img src='"+image+"' alt='...'><div class='caption'>" +"<h3>"+titulo+"</h3>"+"<p><a href='#' class='btn btn-primary' role='button'>Button</a> <a href='#' class='btn btn-default' role='button'>Button</a></p></div></div></div>";
+		   
+		   
+		  $("#newsWrapper").append(Html);
+		  
+	  };
+
+	 
+	  $.each(Noticias, function(i, item) {
+		    //alert(data[i].PageName);
+		  
+		  appendNews(item.col,item.image,item.title);
+		  
+		});
+
+	  
+	  
+	  
+	  $("#ConfigPerfil").click(function(){
+		  
+		  $("#formPerfil").show( "slow");
+		  
+	  });
+	  
 	 $("#hue-demo").minicolors({
 		    changeDelay: 200,
 		    position:'bottom right',
+		    format: 'rgb',
 		    
 		    change: function(value, opacity) {
 		        console.log(value + ' - ' + opacity);
-		        $(".PerfilNews").css("background-color", value);
+		        $("#newsWrapper").css("background-color", value);
+		  
+		        
+		        
+		        
+		       // $('#fondito').children().css('opacity', 1);
 		    }
 		});
 	 $("#BannerColor").minicolors({
